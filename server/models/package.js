1f30a6c71825899212a9d3a07912856531ccd63d
rogers-ch <crogers25@mail.greenriver.edu>
2020-07-20 16:12:28 -0700

    Change data types in server/models/package.js
    Change data types from String to Number for deliveryStatus and checkInStatus in server/models/package.js. Add validation for deliveryStatus and checkInStatus. Add comments including array values for deliveryStatus and checkInStatus.

--- server/models/package.js
+++ server/models/package.js
@@ -14,18 +14,29 @@ const packageSchema = new mongoose.Schema({
   },
 
   deliveryStatus: {
-    type: String,
-    default: "unknown"
+    type: Number,
+    default: 6,   //'unknown'
+
+    //Possible values: ['pre_transit', 'transit', 'out_for_delivery', 'delivered', 
+    // 'return_to_sender', 'failure', 'unknown']
+
+    //NOTE: get the correct number for the API response before making create/update call to db
     
-    //NOTE: add validation after reviewing APIs
-    //validate(value) {
-    //  if (value < 0) throw new Error("Negative calories aren't real.");
-    //}
+    //Validation
+    validate(value) {
+      if (value < 0 || value > 6) throw new Error("Please enter a valid value.");
+    }
   },
 
   checkInStatus: {
-    type: String,
-    default: "not checked in"
+    type: Number,
+    default: 0,   //'not checked in'
+
+    //Possible values: ['not checked in', 'checked in']
+
+    validate(value) {
+      if (value < 0 || value > 1) throw new Error("Please enter a valid value.");
+    }
   },
 
   checkInDate: {
